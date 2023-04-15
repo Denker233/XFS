56071b9751a05eb1d8bf122ab1daf2702cd720f3
TIAN0138 <tian0138@umn.edu>
2023-04-15 15:13:45 -0500

    boot

--- node.c
+++ node.c
@@ -4,10 +4,64 @@
 char server_IP[20];
 int server_port = 8000;
 
+void send_message(char *IP, int Port, char *message)
+{
+    int sockfd;
+    struct addrinfo hints, *servinfo, *p;
+    int rv;
+    int numbytes;
 
-int main(void) {
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_INET; // set to AF_INET to use IPv4
+    hints.ai_socktype = SOCK_DGRAM;
+
+    char port_str[6];
+    sprintf(port_str, "%d", Port);
+
+    if ((rv = getaddrinfo(IP, port_str, &hints, &servinfo)) != 0)
+    {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+        return;
+    }
+
+    // loop through all the results and make a socket
+    for (p = servinfo; p != NULL; p = p->ai_next)
+    {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype,
+                             p->ai_protocol)) == -1)
+        {
+            perror("talker: socket");
+            continue;
+        }
+
+        break;
+    }
+
+    if (p == NULL)
+    {
+        fprintf(stderr, "talker: failed to create socket\n");
+        return;
+    }
+
+    if ((numbytes = sendto(sockfd, message, strlen(message), 0,
+                           p->ai_addr, p->ai_addrlen)) == -1)
+    {
+        perror("talker: sendto");
+        exit(1);
+    }
+
+    freeaddrinfo(servinfo);
+
+    printf("talker: sent %d bytes to %s\n", numbytes, IP);
+    close(sockfd);
+}
+
+void boot(){
     char message[100];
     strcpy(message,"I am fine");
     strcpy(server_IP,"127.0.0.1");
     send_message(server_IP,server_port, message);
+}
+int main(void) {
+    boot()
 }
--- server.c
+++ server.c
@@ -2,6 +2,59 @@
 
 int server_port = 8000;
 
+int bind_udp(int port){
+    int sockfd;
+    struct sockaddr_in cli_addr;
+
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        printf("\n Socket creation error \n");
+        return -1;
+    }
+
+    memset(&cli_addr, '0', sizeof(cli_addr));
+
+    /* Hardcoded IP and Port for every client*/
+    cli_addr.sin_family = AF_INET;
+    cli_addr.sin_port = htons(port);
+    cli_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    /* Bind the socket to a specific port */
+    if (bind(sockfd, (const struct sockaddr *) &cli_addr, sizeof(cli_addr)) < 0) {
+        printf("\nBind failed\n");
+        return -1;
+    }
+
+    /* Set IP and Port */
+    struct sockaddr_in tmp_addr;
+    socklen_t len = sizeof(tmp_addr);
+    getsockname(sockfd, (struct sockaddr *) &tmp_addr, &len);
+    // client_IP = inet_ntoa(tmp_addr.sin_addr);
+    // client_Port = ntohs(tmp_addr.sin_port);
+    
+    // printf("Client IP: %s, Port: %d\n", client_IP, client_Port);
+    return sockfd;
+}
+
+void* receive_udp_message(int arg) {
+    int sockfd = arg;
+    char buf[MAXBUFLEN];
+    struct sockaddr_storage sender_addr;
+    socklen_t addr_len = sizeof(sender_addr);
+    while (1) {
+        int numbytes = recvfrom(sockfd, buf, MAXBUFLEN-1 , 0, (struct sockaddr *)&sender_addr, &addr_len);
+        if (numbytes == -1) {
+            perror("recvfrom");
+            exit(1);
+        }
+        printf("%s",buf);
+        printf("\n");
+        buf[numbytes] = '\0';
+        printf("Received subsribed article from server: %s\n", buf);
+    }
+
+    pthread_exit(NULL);
+}
+
 int main(){
     int sockfd = (bind_udp(server_port));
     receive_udp_message(sockfd);
